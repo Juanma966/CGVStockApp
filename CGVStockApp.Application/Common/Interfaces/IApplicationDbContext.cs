1963dea1a30e33821fc29f46e19e4eef8366b70b
Juanma Oro <juanorodiaz96@gmail.com>
2026-06-13 17:34:54 -0300

    feat(app): creation new interface and efcore install

--- CGVStockApp.Application/Common/Interfaces/IApplicationDbContext.cs
+++ CGVStockApp.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,6 +1,20 @@
+using CGVStockApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace CGVStockApp.Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
+    DbSet<Product> Products { get; }
+    DbSet<Category> Categories { get; }
+    DbSet<Subcategory> Subcategories { get; }
+    DbSet<User> Users { get; }
+    DbSet<Role> Roles { get; }
+    DbSet<Sale> Sales { get; }
+    DbSet<SaleDetail> SaleDetails { get; }
+    DbSet<StockMovement> StockMovements { get; }
+    DbSet<AccountingMovement> AccountingMovements { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     
 }
